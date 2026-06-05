74db58dc5c09e9910c0078d6ae7de93cf62a28e3
ivy <ivyzheng568@gmail.com>
2026-06-05 13:39:28 +0800

    keep trying

--- src/utils.py
+++ src/utils.py
@@ -272,10 +272,11 @@ def plot_embed(
             for phoneme, color in _build_color_map(EMBED_NEW_IDX, COLOR_PALETTE).items()
         }
 
-        fig = plt.figure(figsize=(8, 2.5))
-        fig.subplots_adjust(wspace=0)
+        fig = plt.figure(figsize=(6, 2.5))
         focus_ax = fig.add_subplot(121, projection="3d")
         all_ax = fig.add_subplot(122, projection="3d")
+        focus_ax.set_position([0.02, 0.14, 0.31, 0.76])
+        all_ax.set_position([0.30, 0.14, 0.31, 0.76])
 
         for i in focus_reduced_df.index:
             focus_ax.scatter(
@@ -327,7 +328,7 @@ def plot_embed(
         fig.legend(
             handles=legend_handles,
             loc="center left",
-            bbox_to_anchor=(0.98, 0.5),
+            bbox_to_anchor=(0.68, 0.5),
             ncol=2,
             frameon=False,
             fontsize=5,
